9155565619a002d84fff2e8bbac8cec74a399dcd
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-02-22 17:46:51 -0300

    Created Map

---
+++ Map/map.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var ball = %Ball
+@onready var player = %Player
+
+signal player_lost
+
+func _on_top_body_entered(body):
+	ball.direction.y *= -1
+	
+func _on_bottom_body_entered(body):
+	ball.global_position = player.global_position + Vector2(0, 10)
+	ball.direction = Vector2(randf_range(0.25, 0.75), randf_range(0.25, 1.0)).normalized()
+	player_lost.emit()
+	
+func _flip_ball_horizontally(body):
+	ball.direction.x *= -1
