4d27d991f1b0d7c387ca007fa1bc7c146407456f
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-02-22 18:03:31 -0300

    fix: player movement

--- Player/player.gd
+++ Player/player.gd
@@ -11,10 +11,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	velocity.y = 0.0
-
 	move_and_slide()
 
 func handle_input() -> int:
-	if Input.is_key_pressed(KEY_LEFT): -1
-	elif Input.is_key_pressed(KEY_RIGHT): 1
+	if Input.is_key_pressed(KEY_LEFT): return -1
+	elif Input.is_key_pressed(KEY_RIGHT): return 1
 	return 0 
