dd0fba8ab38f96fe9a7caa7062347a6c1ce4ac1c
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-02-22 18:03:53 -0300

    feat: startup insert bricks

--- game.gd
+++ game.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
 @onready var player = %Player
-
+@onready var map = %Map
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var loaded_brick = preload("res://Brick/brick.tscn")
+	var y_start = 40
+	var x_start = 400
+	for i in range(0,5):
+		for j in range(0, 4):
+			var brick = loaded_brick.instantiate()
+			brick.position = Vector2(x_start, y_start)
+			brick.scale = Vector2(0.8, 0.8)
+			self.add_child(brick)
+			x_start += 100
+		x_start = 400
+		y_start += 35
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
